340a83319d595b3c6c33ebcab61da4aa0a6ac928
Dao Tai <ductaidao24120001@gmail.com>
2024-01-04 21:02:18 +0700

    update

--- lstm.py
+++ lstm.py
@@ -4,11 +4,10 @@ from datetime import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import precision_score, recall_score, f1_score
-from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
 from keras.utils import to_categorical
-# Định nghĩa các trường trong file csv:
+# Định nghĩa các trường :
 """
     id: id người dùng
     school: trường học
@@ -46,10 +45,9 @@ def run_predict(inputData,myAvgScores):
     X = df[LIST_FEATURES].astype('float32')
     Y = df[LABEL]
 
-    # Convert labels to one-hot encoding
+    # Convert labels to one-hot encoding: convert value label to binary vector => Good for model
     Y = to_categorical(Y)
-   
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, shuffle=True )
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, shuffle=True, )
 
     # Xây dựng mô hình LSTM
     # Build the LSTM model
@@ -61,24 +59,21 @@ def run_predict(inputData,myAvgScores):
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # Train the model
-    model.fit(X_train, y_train, epochs=20, batch_size=32, verbose=1, shuffle=True, validation_data=(X_test, y_test))
+    model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=1, shuffle=True, validation_data=(X_test, y_test))
 
-    # #Predict record in test size
     y_pred = model.predict(X_test)
     #  Đánh giá mô hình
     loss, accuracy = model.evaluate(X_test, y_test)
-
-    # print("\t*****Đánh giá mô hình*****")
     print(f'Loss: {loss}, Accuracy: {accuracy}')
 
-    # Chuyển đổi kết quả dự đoán thành nhãn được encode
-    y_pred_encoded = np.argmax(y_pred, axis=1)
-    y_test_encoded = np.argmax(y_test, axis=1)
+    # Chuyển label từ One-hot coding về dạng số: Lấy index có giá trị lớn nhất của từng vector con trong ma trận
+    max_y_pred = np.argmax(y_pred, axis=1)
+    max_y_test = np.argmax(y_test, axis=1)
 
     # Tính các độ đo precision, recall và F1-score
-    precision = precision_score(y_test_encoded, y_pred_encoded, average='weighted', zero_division=0)
-    recall = recall_score(y_test_encoded, y_pred_encoded, average='weighted', zero_division=0)
-    f1 = f1_score(y_test_encoded, y_pred_encoded, average='weighted')
+    precision = precision_score(max_y_test, max_y_pred, average='weighted', zero_division=0)
+    recall = recall_score(max_y_test, max_y_pred, average='weighted', zero_division=0)
+    f1 = f1_score(max_y_test, max_y_pred, average='weighted')
 
     print(f'Precision: {precision}, Recall: {recall}, F1-score: {f1}')
 
@@ -95,29 +90,19 @@ def run_predict(inputData,myAvgScores):
         unseen_vals = dfMyAvgScores[~dfMyAvgScores[column].isin(label_encoder.classes_)][column].unique()
         unseen_values[column] = unseen_vals.tolist()
         if unseen_vals.size > 0:
-            # Lấy giá trị mã hoá lớn nhất tại column đang xét
-            max_label = max(label_encoder.transform(label_encoder.classes_))
-            # Lấy danh sách các lớp hiện tại
+            # Lấy danh sách các lớp hiện tại và thêm dữ liệu unseen vào
             existing_classes = label_encoder.classes_.tolist()
-            # Mở rộng danh sách các lớp
             existing_classes.extend(unseen_vals)
-            # Sắp xếp lại và loại bỏ các giá trị trùng lặp
             existing_classes = sorted(set(existing_classes))
-            # Cập nhật lại danh sách các lớp trong encoder
             label_encoder.classes_ = np.array(existing_classes)
-            # Mã hoá lại cột tương ứng trong dataframe
-            dfMyAvgScores[column] = max_label + 1
-            # print(f"Column {column}: ", dfMyAvgScores[column])
-        else:
-            dfMyAvgScores[column] = label_encoder.transform(dfMyAvgScores[column])
+            
+        dfMyAvgScores[column] = label_encoder.transform(dfMyAvgScores[column])
       
-        
     myFeatures = dfMyAvgScores[LIST_FEATURES].astype('float32')
     myLabels = dfMyAvgScores[LABEL]
     predicted_jobs = []
     for index,row in myFeatures.iterrows():
         decoded_data={}
-        # print("Encode_data: ", row)
         for column in LIST_FEATURES:
             if column in label_encoders:
                 label_encoder = label_encoders[column]
@@ -126,11 +111,9 @@ def run_predict(inputData,myAvgScores):
                 decoded_data[column] = decoded_value
             else:
                 decoded_data[column] = row[column]
-        # print("decoded_data: ",decoded_data, flush=True)
         prediction = model.predict([row])
         predicted_class = np.argmax(prediction, axis=1)[0]
         result = label_encoders['suitable_job_course'].inverse_transform([predicted_class])[0]
-        # print("result: ",result,flush=True)
         predicted_jobs.append(result)
 
     return predicted_jobs
